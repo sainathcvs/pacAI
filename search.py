a6ea46891fa1bbb581832522f29263beada1ff15
Sainath Cvs <sainath.cvs@gmail.com>
2017-09-29 18:53:43 -0400

    final code before submission

--- search.py
+++ search.py
@@ -128,120 +128,15 @@ def extractDirections(problem, goalState, pathToGoal):
 
 def extractDirectionsN(problem, goalState, pathToGoal):
     directions = []
-    for key, value in pathToGoal.items():
-      print key, " ", value
     if goalState in pathToGoal:
         parent = pathToGoal[goalState]
     else:
         return directions
-    print "goalState", goalState
     directions.append(goalState[1])
     while parent in pathToGoal:
         directions.append(parent[1])
         parent = pathToGoal[parent]
-        print "parent ",parent
-    print directions
     return directions[::-1]
-
-
-oD = {"South":"North", "North":"South","West":"East","East":"West"}
-
-
-# Returns LCA if node n1 , n2 are present in the given
-def findLCA(path1, path2, pathToGoal):
-    # To store paths to n1 and n2 fromthe root
-    # Compare the paths to get the first different value
-    path1 = path1[::-1]
-    path2 = path2[::-1]
-    i = 0
-    while (i < len(path1) and i < len(path2)):
-        if path1[i] == path2[i]:
-            break
-        i += 1
-    path1[i]
-    print "least common parent ", path1[i]
-    first = path1[-1]
-    second = path2[-1]
-    parent = pathToGoal[first]
-    directions1 = []
-    while parent[0][0][0] in pathToGoal:
-        directions1.append(parent[1])
-        parent = pathToGoal[parent[0][0][0]]
-    directions1.append(parent[1])
-    directions1 = directions1[::-1]
-    leng = len(directions1)
-    for i in xrange(leng):
-        directions1.append(oD[directions1[leng-i-1]])
-
-    directions2 = []
-    parent = pathToGoal[second]
-    while parent[0][0][0] in pathToGoal:
-        directions2.append(parent[1])
-        parent = pathToGoal[parent[0][0][0]]
-    directions2.append(parent[1])
-    directions2 = directions2[::-1]
-    leng = len(directions2)
-    for i in xrange(leng):
-        directions2.append(oD[directions2[leng-i-1]])
-
-    print "directions1 ",directions1,"directions2 ",directions2
-    return directions1[:]+directions2[:]
-
-def extractDirectionsCorners(problem, goalState, pathToGoal):
-    corners = goalState[0][1]
-    directions = []
-    print "path to goal", problem.corners_visited
-    if goalState[0][0] in pathToGoal:
-        print "goal---", goalState
-        parent = pathToGoal[goalState[0][0]]
-    else:
-        return directions
-    print "pathToGoal ",pathToGoal
-    li = range(len(corners))
-    #li[0:len(corners):2]
-    for i in li[0:len(corners):2]:
-        first = corners[i]
-        second = corners[i+1]
-        firstPath = [first]
-        secondPath = [second]
-        parent = pathToGoal[first]
-        print "parent", parent
-        while parent[0][0][0] in pathToGoal:
-            print "par---",parent[0][0],"---",parent[1]
-            firstPath.append(parent[0][0][0])
-            parent = pathToGoal[parent[0][0][0]]
-        firstPath.append(parent[0][0][0])
-        parent = pathToGoal[second]
-        while parent[0][0][0] in pathToGoal:
-            print "par---",parent[0][0],"---",parent[1]
-            secondPath.append(parent[0][0][0])
-            parent = pathToGoal[parent[0][0][0]]
-        secondPath.append(parent[0][0][0])
-        print "Iteration i",i, " firstPath ", firstPath, "secondPath ",secondPath
-        temp = findLCA(firstPath, secondPath, pathToGoal)
-        directions+= temp[:]
-
-    print "directions ", directions
-    return directions
-
-
-def extractDirectionsCorners1(problem, goalState, pathToGoal):
-    directions = []
-    print "path to goal", problem.corners_visited
-    if goalState[0][0] in pathToGoal:
-        print "goal---", goalState
-        parent = pathToGoal[goalState[0][0]]
-    else:
-        return directions
-    while parent[0][0][0] in pathToGoal:
-        print "par---",parent[0][0],"---",parent[1]
-        directions.append(parent[1])
-        parent = pathToGoal[parent[0][0][0]]
-    print "directions ", directions, "parent[0][1] ",parent
-    directions.append(parent[1])
-    print "directions[::-1]", directions[::-1]
-    return directions[::-1]
-
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
@@ -249,7 +144,6 @@ def breadthFirstSearch(problem):
     # Get start state, push it to stack and mark it as visited.
     start_state = problem.getStartState()
     startState = (start_state,'','')
-    print startState
     fringeList = util.Queue()
     fringeList.push(startState)
     visitedList = {}
@@ -261,14 +155,11 @@ def breadthFirstSearch(problem):
         
         if currState[0] in visitedList:
           continue
-        print "currState ",currState
         visitedList[currState[0]] = 1
         if problem.isGoalState(currState[0]):
-          print "Goal reached ",currState
           return extractDirectionsN(problem, currState, pathToGoal)
 
         successors = problem.getSuccessors(currState[0])
-        print "successors ",successors, "\n"
         for sc in successors:
             # push all the nodes that are not visited into Queue
             if sc[0] not in visitedList:
@@ -290,9 +181,7 @@ def uniformCostSearch(problem):
     while fringeList.isEmpty() == False:
         #Pop the first node from Queue
         currState = fringeList.pop()
-        print "currentState", currState
         if currState[0] in visitedList:
-          print "currState ",currState, "visited"
           continue
 
         visitedList[currState[0]]=1        
@@ -331,9 +220,7 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     while fringeList.isEmpty() == False:
         #Pop the first node from Queue
         currState = fringeList.pop()
-        print "currentState", currState
         if currState[0] in visitedList:
-          print "currState ",currState, "visited"
           continue
 
         visitedList[currState[0]]=1        
@@ -356,4 +243,3 @@ bfs = breadthFirstSearch
 dfs = depthFirstSearch
 astar = aStarSearch
 ucs = uniformCostSearch
-
